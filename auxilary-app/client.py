57356c3a9e5437fb3136a7c26ed6e482b3bb9299
vasanthbalguri <vasanthbalguri@gmail.com>
2021-11-27 23:51:45 +0530

    tested gcode-generator :)

--- auxilary-app/client.py
+++ auxilary-app/client.py
@@ -9,10 +9,15 @@ slicer = Slic3rModule.Slic3rModule()
 @app.route('/slice',methods = ['POST'])
 def slice():
    if request.method == 'POST':
-    slicer.addModel(request.form.stlPath)
-    slicer.slice(request.form.name + ".gcode",0,0)
-    os.replace(request.form.name + ".gcode" , "../gcodes/" + request.form.name + ".gcode")
-    return request.form.name + ".gcode"
+    #slicer.addModel(request.form.stlPath)
+    print(request.json["filepath"])
+    filepath = request.json["filepath"]
+    outpath = "gcodes/" +  request.json["name"] + ".gcode"
+    print(outpath)
+    slicer.addModel(filepath)
+    slicer.slice(outpath ,0,0)
+    #os.replace(request.form.name + ".gcode" , "../gcodes/" + request.json.name + ".gcode")
+    return outpath
    else:
       return 'GET not supported'
 
--- iiot-server/client.py
+++ iiot-server/client.py
@@ -6,8 +6,6 @@ import sys
 import time
 import threading
 import json
-import configuration
-import json
 
 from printrun.printcore import printcore
 from printrun.utils import setup_logging
@@ -25,36 +23,49 @@ temprature = ""
 enable = 0
 idle = 0
 state = "idle"
-
+task = "null"
+machineid = ""
 p = printcore(port, baud)
 p.loud = True
 p.tempcb = temp_callback
 
 class MonitorThread (threading.Thread):
-   def __init__(self, threadID, name):
-      threading.Thread.__init__(self)
-      self.threadID = threadID
-      self.name = name
-   def run(self):
-      print "Starting monitoring " + self.name
-      while enable:
-        monitor()
-      print "Exiting monitoring" + self.name
-
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        print("Starting monitoring " + self.name)
+        while enable:
+            monitor()
+        print("Exiting monitoring" + self.name)
+'''
+data sent
+{
+id:machineid
+temprature:temprature
+progress:number
+status:idle/printing/printed
+task:taskid
+frame:image
+}
+'''
 def monitor():
     frame = ""
     data = ""
+    progress = 0
+    global state
+    global task
     if(p.printing):
-        progress = 0
         if(p.mainqueue != "None"):
             progress = 100 * float(p.queueindex) / len(p.mainqueue)
-            p.send_now("M105")
-            #sio.emit('iiot-video-feed',"data:image/jpeg;base64,"+frame+"==")
-            data = "{"+"temprature:" + temprature + ",progress:" + progress + ",status:" + state)
+        p.send_now("M105")
+        data = "{"+ "id:" + machineid + ",temprature:" + temprature + ",progress:" + progress + ",status:" + state  + ",task:" + task
+    elif state == "idle":
+        data = "{"+ "id:" + machineid + ",temprature:" + temprature + ",progress:" + progress + ",status:" + state + ",task:null" 
     else:
-        global state
         state = "printed"
-        data = "{"+"temprature:" + temprature + ",progress:" + progress + ",status:" + state
+        data = "{"+ "id:" + machineid + ",temprature:"  + temprature + ",progress:" + progress + ",status:" + state + ",task:" + task
         print("finished")
     if(cap.isOpened()):
         ret,img=cap.read()
@@ -63,7 +74,7 @@ def monitor():
             frame = cv2.imencode('.jpg', img)[1].tobytes()
             frame= base64.encodebytes(frame).decode("utf-8")
             
-    sio.emit('getIiotData',data + ",frame:" frame "}")
+    sio.emit('getIiotData',data + ",frame:" + frame + "}")
     time.sleep(1/60)
 
 m = Monitorthread()
@@ -81,6 +92,88 @@ def connect():
 def connect_error(data):
     print("The connection failed!")
     print(data)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/connect',methods = ['GET','POST'])
+def connect():
+    if request.method == 'GET':
+        return 'available'
+    if request.method == 'POST':
+       global enable
+       global machineid
+       enable = 1
+       machineId = request.json["machineId"]
+       #slicer.addModel(request.form.stlPath)
+       #slicer.slice("out.gcode",0,0)
+       sio.connect(request.json["serverLink"])
+       sio.emit("init-iiot-endpoint",request.json["machineId"])
+       m.start()
+       return 'connecting'
+'''
+{
+taskid:string
+gcode:string
+}
+'''
+@app.route('/print', methods = ['POST'])
+def print():
+    if request.method == 'POST':
+       # gcode = request.files['out.gcode']
+        gcode = request.json['gcode']
+        if(not p.printing):
+            #gcode = [i.strip() for i in open(filename)]
+            global idle
+            global state
+            global task
+            idle = 0
+            gcode = gcoder.LightGCode(gcode)
+            p.startprint(gcode)
+            state = "printing"
+            task = request.taskid
+            return "printing"
+        else:
+            return "busy"
+
+@app.route('/finishPrint')
+def finishPrint():
+    global state
+    global task
+    state = "idle"
+    task = null
+    return 'finished_print'
+
+@app.route('/disconnect',methods = ['DELETE'])
+def disconnect():
+    if request.method == 'DELETE':
+        enable = 0
+        m.join()
+        sio.disconnect()
+        cap.release()
+        return "disconnected"
+#not used
+@app.route('/enableMonitor')
+def enableMonitor():
+    global idle
+    idle = 0
+    return 'monitoring_enabled'
+
+#not used    
+@app.route('/disableMonitor')
+def disbaleMonitor():
+    global idle
+    idle = 1
+    return 'monitoring_disabled'
+
+def temp_callback(a):
+    global temprature
+    temprature = a.split()[1][2:]
+    return temprature
+
+#sio.connect("http://192.168.0.5:3000")
+
 
 
 #not used
@@ -123,70 +216,4 @@ def disconnect():
 	print("DISCONNECTED")
 '''
 #not used
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/connect',methods = ['POST'])
-def connect():
-   if request.method == 'POST':
-    #slicer.addModel(request.form.stlPath)
-    #slicer.slice("out.gcode",0,0)
-    sio.connect(request.link)
-    sio.emit("init-iiot-endpoint",request.id)
-    #m.start()
-    return 'connecting'
-   else:
-      return 'GET not supported'
-#not used
-@app.route('/enableMonitor')
-def enableMonitor():
-    global idle
-    idle = 0
-    return 'monitoring_enabled'
-
-@app.route('/print', methods = ['POST'])
-def print():
-    if request.method == 'POST':
-        gcode = request.files['out.gcode']
-        if(not p.printing):
-            #gcode = [i.strip() for i in open(filename)]
-            global idle
-            global state
-            idle = 0
-            gcode = gcoder.LightGCode(gcode)
-            p.startprint(gcode)
-            state = "printing"
-            return "printing"
-        else:
-            return "busy"
-#not used    
-@app.route('/disableMonitor')
-def disbaleMonitor():
-    global idle
-    idle = 1
-    return 'monitoring_disabled'
-    
-@app.route('/finishPrint')
-def finishPrint():
-    global state
-    state = "idle"
-    return 'finished_print'
-
-@app.route('/disconnect',methods = ['DELETE'])
-def disconnect():
-    if request.method = 'DELETE'
-    enable = 0
-    m.join()
-    sio.disconnect()
-	cap.release()
-	retrun "disconnected"
-
-def temp_callback(a):
-    global temprature
-    temprature = a.split()[1][2:]
-    return temprature
-
-#sio.connect("http://192.168.0.5:3000")
-
 
